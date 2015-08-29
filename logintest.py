fdde834f5a1f181d2457ae8d0cdd6a96595ed780
alenX <genhaoai@gmail.com>
2015-08-29 10:23:52 +0800

    新增添加页面以及主页面内容调整

--- logintest.py
+++ logintest.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
 from flask import request
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TRARDOWN'] = True
+db = SQLAlchemy(app)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -14,7 +20,7 @@ def login():
 
 @app.route("/index/<username>")
 def index(username):
-    return render_template('index.html',username=username)
+    return render_template('index.html', username=username)
 
 
 @app.route('/')
@@ -22,7 +28,21 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/save')
+def save_user():
+    print 'dd'
 if __name__ == '__main__':
     app.run()
 
 
+class User(db.Model):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.column(db.String(100), unique=True)
+    telephone = db.column(db.String(20))
+    email = db.column(db.String(20))
+    password = db.column(db.String(20))
+    validate = db.column(db.Boolean)
+
+    def __repr__(self):
+        return '<User %r>' %self.username
